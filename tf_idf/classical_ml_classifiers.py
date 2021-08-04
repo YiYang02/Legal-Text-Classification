288f04afcaa8f382b7655e63058d1cce2dc533a7
Yi Yang <yyang2@bowdoin.edu>
2021-08-04 00:41:14 -0400

    Added ml results for sent and doc level. Added processed doc and sent level csv files

--- tf_idf/classical_ml_classifiers.py
+++ tf_idf/classical_ml_classifiers.py
@@ -1,85 +1,106 @@
+import lightgbm
 import nltk
 from process_clauses_from_mastercsv import *
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.multiclass import OneVsOneClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn import tree
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
 
+# Check for overfitting using this guide: https://www.kaggle.com/prashant111/lightgbm-classifier-in-python
 
-# No Pre-Processing on Sentences - Accuracy: 0.7395377888819488
-# Pre-Processing on Sentences - Accuracy: 0.7282948157401624
-def tf_idf_randomforest(list_of_sentences, labels):
+def tf_idf_representation(list_of_sentences, labels):
+    # Bag of Words
     vectorizer = CountVectorizer(max_features=20000, ngram_range=(1, 3), min_df=0.01, max_df=0.8,
                                  stop_words=stopwords.words('english'))
     X = vectorizer.fit_transform(list_of_sentences).toarray()
     Y = labels
+    # TF-IDF
     tfidfconverter = TfidfTransformer()
     X = tfidfconverter.fit_transform(X).toarray()
-    print(type(X))
-    print(X)
-    print(type(Y))
-    print(Y)
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-    classifier = RandomForestClassifier(n_estimators=3000, random_state=0)
+    # print(type(X))
+    # print(X)
+    # print(type(Y))
+    # print(Y)
+
+    print("tf-idf completed")
+
+    return X, Y
+
+
+def modelfitting(classifier):
+    cv_rf = cross_validate(classifier, X_train, y_train, cv=10)
+    # print("Cross validation accuracy mean score: ", cv_rf['test_score'].mean())
     classifier.fit(X_train, y_train)
+
     y_pred = classifier.predict(X_test)
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred))
+    confusion_m = confusion_matrix(y_test, y_pred)
+    # print("Confusion Matrix: ", confusion_m, "\n")
+    classification_r = classification_report(y_test, y_pred)
+    # print("Classification Report: ", classification_r, "\n")
+    accuracy_s = accuracy_score(y_test, y_pred, "\n")
+    # print("Accuracy Score: ", accuracy_s)
+
+    with open(r'C:\Users\yiyan\Desktop\Legal Text Classification\tf_idf\classifiers_results4.txt', 'a') as text_file:
+        text_file.write('\n')
+        text_file.write("Classification Report: " + '\n' + str(classification_r) + '\n')
+        text_file.write("Accuracy Score: " + str(accuracy_s) + '\n')
+        text_file.write("Cross validation accuracy mean score: " + str(cv_rf['test_score'].mean()) + "\n")
+        text_file.write("=========================================================" + '\n')
+
+    print("Model Fitting completed")
 
 
-# four model: RandomForestClassifier,LinearSVC,MultinomialNB,LogisticRegression
-def multimodels(document_list, labels):
-    unique_labels = list(set(labels))
-    vectorizer = CountVectorizer(max_features=20000, ngram_range=(1, 3), min_df=0.01, max_df=0.8,
-                                 stop_words=stopwords.words('english'))
-    features = vectorizer.fit_transform(document_list).toarray()
-    X = vectorizer.fit_transform(document_list).toarray()
-    Y = labels
-    tfidfconverter = TfidfTransformer()
-    X = tfidfconverter.fit_transform(X).toarray()
-    print(X)
-    print(Y)
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-    clf = MultinomialNB().fit(X_train, y_train)
-
-    models = [
-        RandomForestClassifier(n_estimators=5000, random_state=0),
-        LinearSVC(max_iter=5000),
-        MultinomialNB(),
-        LogisticRegression(solver='lbfgs', multi_class='auto',random_state=0),
-    ]
-    CV = 5
-    cv_df = pd.DataFrame(index=range(CV * len(models)))
-    entries = []
-    for model in models:
-        model_name = model.__class__.__name__
-        accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
-        for fold_idx, accuracy in enumerate(accuracies):
-            entries.append((model_name, fold_idx, accuracy))
-    cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
-
-    sns.boxplot(x='model_name', y='accuracy', data=cv_df)
-    sns.stripplot(x='model_name', y='accuracy', data=cv_df,
-                  size=8, jitter=True, edgecolor="gray", linewidth=3)
-    plt.show()
-    performance = cv_df.groupby('model_name').accuracy.mean()
-    print(performance)
-
+# # C4.5
+# def c45_model():
+#     # In second key, 'Decision' is equal to 'Sentence ID'
+#     dict_sentences_to_label = {'Sentence Text': list_of_sentences, 'Decision': labels}
+#     df = pd.DataFrame(dict_sentences_to_label)
+#     config = {'algorithm': 'C4.5', 'enableParallelism': True, 'num_cores': 2}
+#     model = chef.fit(df, config=config)
+#     return
 
 if __name__ == '__main__':
-    result = extract_and_create_new_csv()
+    doc_result = extract_and_create_documentlevel_csv()
+    # sent_result = extract_and_create_sentencelevel_csv()
     # for item in result[0]:
     #     print("Item: ", item)
-    tf_idf_randomforest(result[0], result[1])
+
+    # list_of_sentences = sent_result[0]
+    # sent_labels = sent_result[3]
+
+    list_of_text = doc_result[0]
+    doc_labels = doc_result[2]
+
+    X, Y = tf_idf_representation(list_of_sentences=list_of_text, labels=doc_labels)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+    nb_classifier = MultinomialNB()
+    knn_classifier = KNeighborsClassifier()
+    cart_classifier = tree.DecisionTreeClassifier()
+    # C-Support Vector Classification. Implements the "One-Versus-One" approach
+    svc_classifer = SVC()
+    # Implements the "One-Versus-Rest" approach
+    linearsvc_classifier = LinearSVC()
+    rf_classifier = RandomForestClassifier(n_estimators=3000, random_state=0)
+    # c45_model()
+    xgboost_classifier = xgb.XGBClassifier(objective='multi:softmax')
+    lightgbm_classifier = lgb.LGBMClassifier()
+
+    modelfitting(classifier=lightgbm_classifier)
+
--- tf_idf/process_clauses_from_mastercsv.py
+++ tf_idf/process_clauses_from_mastercsv.py
@@ -50,7 +50,7 @@ CONTRACT_CATEGORIES = {'Affiliate Agreement': 0,
                            }
 
 # File path to access master_clauses.csv file
-masterclauses_filepath = r'C:\Users\yiyan\Desktop\CUAD_v1\master_clauses.csv'
+masterclauses_filepath = r'C:\Users\yiyan\Desktop\Legal Text Classification\master_clauses.csv'
 
 fileid_to_filenames = pd.read_csv(masterclauses_filepath, usecols=['Filename'])
 # Dictionary: key - unique number identifier; value - legal contract file name (string)
@@ -153,7 +153,7 @@ def extract_headerrow_from_csvfile(filepath: str):
     # Dictionary of all the possible categories a legal contract can be in
     header_cols_dict = header_cols.to_dict(orient='dict')['Category (incl. context and answer)']
 
-    # Dictionary with key (clause_id: 0-41) and value (clause label: "Document Name, Parties, Agreement Date, etc')
+    # Dictionary with key (clause_id: 0-40) and value (clause label: "Document Name, Parties, Agreement Date, etc')
     clauseid_clauses_dict = {}
     for key in header_cols_dict:
         value = header_cols_dict[key].split(":")[-1].strip()
@@ -176,10 +176,7 @@ def inverted_dictionary(dictionary: dict):
     Returns:
         Iterable[dict]: inverted dictionary
     """
-    ret_dict = {}
-
-    for key, value in dictionary.items():
-        ret_dict[value] = key
+    ret_dict = {value: key for key, value in dictionary.items()}
 
     return ret_dict
 
@@ -199,92 +196,9 @@ def extract_clausesentences_cols(filepath: str, clauses_cols: list):
     return list_of_dict_clausesentences_to_fileid
 
 
-def extract_and_create_new_csv(preprocess: bool):
-    """
-    Creates a new CSV file with four columns: Sentence Text, Sentence ID, File ID, and Category ID.
-
-        Sentence Text rows are made by extracting all the clause columns (41) from master csv. Each sentence text is kept
-        in its original form as found in their respective legal contract. Sentences are also preprocessed to extract
-        features.
-
-        Sentence ID rows are made by vectoring the clause a sentence belongs to into a number.
-
-        File ID rows are made by vectoring the file name a clause comes from
-
-        Category ID rows are made by vectoring the legal contract domain that the clause comes from
-
-
-    Returns three lists:
-        List #1 contains all rows from Sentence Text column
-        List #2 contains all rows ids from Clause ID column
-        List #3 contains all rows from Category ID column
-    """
-
-    dict_fileid_to_categoryid = convert_dictval_to_id(dictionary=DICT_FILEID_TO_RAWCATEGORY)
-    tuple_result = extract_headerrow_from_csvfile(filepath=r'C:\Users\yiyan\Desktop\CUAD_v1\tf_idf\catergory_descriptions.csv')
-    clauses = list(tuple_result[1])
-    list_of_dict_clausesentences_to_fileid = extract_clausesentences_cols(
-        filepath=masterclauses_filepath, clauses_cols=clauses)
-    dict_clauses_to_clauseid = inverted_dictionary(tuple_result[0])
-
-    output_dict = {"Sentence Text": [], "Sentence ID": [], "File ID": [], "Category ID": []}
-
-    # Lists to be returned for tf-idf rf classification
-    all_sentences = []
-    all_sentenceid = []
-    all_categories = []
-
-    index = 0
-    for dict in list_of_dict_clausesentences_to_fileid:
-        simplified_dict = dict.to_dict()[clauses[index]]
-        for key, value in simplified_dict.items():
-            if key in dict_fileid_to_categoryid:
-                # Remove rows that do not have any sentences
-                if preprocess == True:
-                    processed_str = preprocessing(value)
-                else:
-                    processed_str = value
-
-                if len(processed_str) > 0:
-                    if len(dict_fileid_to_categoryid[key]) == 1:
-                        output_dict["Sentence Text"].append(processed_str)
-                        output_dict["Sentence ID"].append(dict_clauses_to_clauseid[clauses[index]])
-                        output_dict["File ID"].append(key)
-                        output_dict["Category ID"].append(dict_fileid_to_categoryid[key][0])
-
-                        all_sentences += [processed_str]
-                        all_sentenceid += [dict_clauses_to_clauseid[clauses[index]]]
-                        all_categories += [dict_fileid_to_categoryid[key][0]]
-
-                        # Create multiple rows if a sentence has multiple categories
-                    else:
-                        for category in dict_fileid_to_categoryid[key]:
-                            output_dict["Sentence Text"].append(processed_str)
-                            output_dict["Sentence ID"].append(dict_clauses_to_clauseid[clauses[index]])
-                            output_dict["File ID"].append(key)
-                            output_dict["Category ID"].append(category)
-
-                            all_sentences += [processed_str]
-                            all_sentenceid += [dict_clauses_to_clauseid[clauses[index]]]
-                            all_categories += [dict_fileid_to_categoryid[key][0]]
-
-        index += 1
-
-    # Debugging
-    # print("Sent Text", len(output_dict["Sentence Text"]))
-    # print("Sent ID", len(output_dict["Sentence ID"]))
-    # print("File ID", len(output_dict["File ID"]))
-    # print("Category ID", len(output_dict["Category ID"]))
-
-    # Writing output to a csv file
-    df = pd.DataFrame(output_dict)
-    df.to_excel(r'C:\Users\yiyan\Desktop\CUAD_v1\tf_idf\tf_idf_data.xlsx')
-
-    return [all_sentences, all_sentenceid, all_categories]
-
-
 def preprocessing(raw_sentence):
     """
+    Helper method for extact_and_create_new_csv().
     Takes in an unprocessed string representing a sentence and processing it classic ml classifiers
     """
     # Set of stop words in English
@@ -320,6 +234,121 @@ def preprocessing(raw_sentence):
     return processed_sentence
 
 
+def extract_and_create_sentencelevel_csv():
+    """
+    Creates a new CSV file with four columns: Sentence Text, Sentence ID, File ID, and Category ID.
+
+        Sentence Text column rows are made by extracting all the clause columns (41) from master csv. Each sentence text is kept
+        in its original form as found in their respective legal contract. Sentences are also preprocessed to extract
+        features.
+
+        Sentence ID column rows are made by vectoring the clause a sentence belongs to into a number.
+
+        File ID column rows are made by vectoring the file name a clause comes from
+
+        Category ID column rows are made by vectoring the legal contract domain that the clause comes from
 
 
+    Returns three lists:
+        List #1 contains all rows from Sentence Text column
+        List #2 contains all rows ids from Clause ID column
+        List #3 contains all rows from Category ID column
+    """
 
+    dict_fileid_to_categoryid = convert_dictval_to_id(dictionary=DICT_FILEID_TO_RAWCATEGORY)
+    tuple_result = extract_headerrow_from_csvfile(filepath=r'C:\Users\yiyan\Desktop\Legal Text Classification\tf_idf\category_descriptions.csv')
+    clauses = list(tuple_result[1])
+    list_of_dict_clausesentences_to_fileid = extract_clausesentences_cols(
+        filepath=masterclauses_filepath, clauses_cols=clauses)
+    dict_clauses_to_clauseid = inverted_dictionary(tuple_result[0])
+
+    output_dict = {"Sentence Text": [], "Sentence ID": [], "File ID": [], "Category ID": []}
+
+    index = 0
+    for dict in list_of_dict_clausesentences_to_fileid:
+        simplified_dict = dict.to_dict()[clauses[index]]
+        for key, value in simplified_dict.items():
+            if key in dict_fileid_to_categoryid:
+
+                processed_str = preprocessing(value)
+
+                # Remove rows that do not have any sentences
+                if processed_str != '[]':
+                    if len(processed_str) > 0:
+                        if len(dict_fileid_to_categoryid[key]) == 1:
+                            output_dict["Sentence Text"].append(processed_str)
+                            output_dict["Sentence ID"].append(dict_clauses_to_clauseid[clauses[index]])
+                            output_dict["File ID"].append(key)
+                            output_dict["Category ID"].append(dict_fileid_to_categoryid[key][0])
+
+                        # Create multiple rows if a sentence has multiple categories
+                        else:
+                            for category in dict_fileid_to_categoryid[key]:
+                                output_dict["Sentence Text"].append(processed_str)
+                                output_dict["Sentence ID"].append(dict_clauses_to_clauseid[clauses[index]])
+                                output_dict["File ID"].append(key)
+                                output_dict["Category ID"].append(category)
+
+        index += 1
+
+    # Debugging
+    # print("Sent Text", len(output_dict["Document Text"]))
+    # print("Sent ID", len(output_dict["Sentence ID"]))
+    # print("File ID", len(output_dict["File ID"]))
+    # print("Category ID", len(output_dict["Category ID"]))
+
+    # Writing output to a csv file
+    # df = pd.DataFrame(output_dict)
+    # df.to_excel(r'C:\Users\yiyan\Desktop\Legal Text Classification\tf_idf\tf_idf_sentence_level.xlsx')
+
+    # Lists to be returned for tf-idf rf classification
+    all_sentences = output_dict["Sentence Text"]
+    all_sentenceid = output_dict["Sentence ID"]
+    all_fileid = output_dict["File ID"]
+    all_categories = output_dict["Category ID"]
+    return [all_sentences, all_sentenceid, all_fileid, all_categories]
+
+
+def extract_and_create_documentlevel_csv():
+    """
+    Creates a new CSV file with three columns: Document Text, File ID, and Category ID.
+
+    Returns two lists:
+        List #1 contains all rows from Document Text column
+        List #2 contains all rows from Category ID column
+    """
+    result = extract_and_create_sentencelevel_csv()
+    all_sentences = result[0]
+    all_fileid = result[2]
+
+    df = pd.DataFrame({"Document Text": all_sentences,
+                      "File ID": all_fileid})
+    df = df.groupby("File ID", as_index=False)["Document Text"].apply(' '.join)
+
+    dict_fileid_to_categoryid = convert_dictval_to_id(dictionary=DICT_FILEID_TO_RAWCATEGORY)
+
+    expanded_list_doctext_col = []
+    expanded_list_fileid_col = []
+    expanded_list_category_col = []
+
+    # index is type series and goes from 0-502, row is type series and gives access to each column's row
+    for index, row in df.iterrows():
+        # Gets the document's File ID and checks how many categories the file belongs to
+        if len(dict_fileid_to_categoryid[row["File ID"]]) == 1:
+            expanded_list_doctext_col.append(row["Document Text"])
+            expanded_list_fileid_col.append(row["File ID"])
+            expanded_list_category_col.append(dict_fileid_to_categoryid[row["File ID"]][0])
+
+        # Create multiple rows if document text has multiple categories
+        else:
+            for category in dict_fileid_to_categoryid[row["File ID"]]:
+                expanded_list_doctext_col.append(row["Document Text"])
+                expanded_list_fileid_col.append(row["File ID"])
+                expanded_list_category_col.append(category)
+
+    # df = pd.DataFrame(list(zip(expanded_list_doctext_col, expanded_list_fileid_col, expanded_list_category_col)),
+    #                   columns=["Document Text", "File ID", "Category ID"])
+
+    # df.to_excel(r'C:\Users\yiyan\Desktop\Legal Text Classification\tf_idf\tf_idf_document_level.xlsx')
+
+    return [expanded_list_doctext_col, expanded_list_fileid_col, expanded_list_category_col]
